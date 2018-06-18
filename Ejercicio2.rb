d360e852f2c6be156c038165e94cb268adabec3d
Olmos Soto Manuel Alejandro <MSOLMOS@entel.cl>
2018-06-18 13:01:20 -0400

    Exercise 2 done

--- Ejercicio2.rb
+++ Ejercicio2.rb
@@ -1,2 +1,10 @@
 #1. Crear un método que lea el archivo, lo abra y devuelva la cantidad de líneas que
 #   posee.
+
+def linesReader(filename)
+    file = File.open(filename, ‘r’)
+    data = file.readlines
+    file.close
+end
+
+linesReader('txtfolder/peliculas.txt')
